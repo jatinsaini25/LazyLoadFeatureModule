292b61162e0e0ec32f8e101bb8a0d1b7de88615f
jatinsaini249 <62991841+jatinsaini249@users.noreply.github.com>
2020-07-08 01:49:42 -0400

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'user', loadChildren: () => import(`./user-profile/user-profile.module`).then(m => m.UserProfileModule) }
+]
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes, {
+      useHash: true,
+      preloadingStrategy: PreloadAllModules
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
